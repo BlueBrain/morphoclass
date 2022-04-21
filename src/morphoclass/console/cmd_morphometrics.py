bda8cca983e09b2829bfcd7055f4ba30579a6b55
Francesco Casalegno <francesco.casalegno@gmail.com>
2022-04-21 15:07:07 +0200

    Add morphometrics feature-extraction [WIP] and config file for morphometrics

---
+++ src/morphoclass/console/cmd_morphometrics.py
@@ -0,0 +1,131 @@
+# Copyright © 2022 Blue Brain Project/EPFL
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Implementation of the `morphoclass morphometrics` CLI command."""
+from __future__ import annotations
+
+import logging
+import os
+import pathlib
+
+import yaml
+
+import click
+
+logger = logging.getLogger(__name__)
+
+
+@click.group(name="evaluate")
+def cli():
+    """Run the morphometrics subcommand."""
+    pass
+
+
+@cli.command(
+    name="extract-features",
+    short_help="Extract morphometrics features",
+)
+@click.option(
+    "--organised-dataset-directory",
+    "organised_dataset_dir",
+    type=click.Path(dir_okay=True, file_okay=False, exists=True),
+    help="Where to read final preprocessed morphology data.",
+    required=True,
+)
+@click.option(
+    "--morphometrics-config",
+    "morphometrics_config_file",
+    typee=click.Path(dir_okay=False, file_okay=True, exists=False),
+    help="Where to read the YAML morphometric feature configurations.",
+    required=True,
+)
+@click.option(
+    "-o",
+    "--output-features-directory",
+    "output_features_dir",
+    type=click.Path(dir_okay=True, file_okay=False, exists=False),
+    help="Where to write extracted morphometrics features.",
+    required=True,
+)
+def extract_features(
+    organised_dataset_dir, morphometrics_config_file, output_features_dir,
+) -> None:
+    """Run the `morphometrics extract-features` subcommand."""
+    logger.info("Start extracting morphometrics features.")
+    logger.info(f"Input organised data path  : {organised_dataset_dir}")
+    logger.info(f"Morphometrics config       : {morphometrics_config_file}")
+    logger.info(f"Output features path       : {output_features_dir}")
+
+    logger.debug("Read morphometrics config file.")
+    with open(morphometrics_config_file) as fh:
+        morphometrics_config = yaml.safe_load(fh)
+    logger.debug(f"Morphometrics configurations: {morphometrics_config}")
+
+    for layer_dir in sorted(f for f in organised_dataset_dir.iterdir() if f.is_dir()):
+        logger.info(f"Process morphologies in layer {layer_dir}")
+
+        dataset_csv = layer_dir / "dataset.csv"
+        if not dataset_csv.is_file():
+            raise click.ClickException(f"No dataset.csv file found in {layer_dir}")
+
+        # df = pd.read_csv(dataset_csv, names=["filepath", "m_type"])
+        # paths = [layer_dir / file_path for file_path in df["filepath"]]
+        #
+        # population = neurom.load_morphologies(paths)
+        # df_metrics = morph_stats.extract_dataframe(population, MORPHOMETRICS_CONFIG)
+        # df_metrics.columns = df_metrics.columns.map("|".join).str.strip("|")
+        #
+        # df["property|name"] = df["filepath"].str.split("/").str[-1]
+        # df_metrics = df_metrics.merge(df, how="inner", on="property|name")
+        #
+        # output_file = OUTPUT_DIR / f"morphometrics_{dataset}_{layer_dir.stem}.csv"
+        # df_metrics.to_csv(output_file, index=False)
+
+        logger.info(f"Done!")
+
+
+@cli.command(
+    name="train",
+    short_help="Train classification model based on morphometrics.",
+)
+@click.argument("checkpoint_path", type=click.Path(dir_okay=False, exists=True))
+@click.argument("report_path", type=click.Path(dir_okay=False, exists=False))
+def train():
+    # """Run the `evaluate latent-features subcommand."""
+    # logger.info("Setting up paths")
+    # checkpoint_path = pathlib.Path(checkpoint_path)
+    # report_path = pathlib.Path(report_path)
+    # if not report_path.suffix.lower() == ".html":
+    #     raise click.ClickException('The report path must have the extension ".html"')
+    #
+    # logger.info("Loading libraries")
+    # from morphoclass import cleanlab
+    #
+    # if not cleanlab.check_installed():
+    #     how_to_install = cleanlab.how_to_install_msg()
+    #     raise click.ClickException(f"CleanLab not installed. {how_to_install}")
+    # from morphoclass.console.evaluate import visualize_latent_features
+    # from morphoclass.training.training_log import TrainingLog
+    #
+    # logger.info("Loading the checkpoint")
+    # training_log = TrainingLog.load(checkpoint_path)
+    #
+    # logger.info("Running CleanLab analysis")
+    # errors, self_confidence = cleanlab.analysis(
+    #     training_log.targets, training_log.probas
+    # )
+    #
+    # logger.info("Generating the report")
+    # visualize_latent_features(training_log, errors, self_confidence, report_path)
+
+    logger.info("Done.")
