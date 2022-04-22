a07ac2eb2d10bed22348f4bf60f1cf9d085f33b7
Francesco Casalegno <francesco.casalegno@gmail.com>
2022-04-22 11:34:51 +0200

    Add debugging and info logs

--- src/morphoclass/console/cmd_morphometrics.py
+++ src/morphoclass/console/cmd_morphometrics.py
@@ -182,7 +182,9 @@ def train(
     from morphoclass.training.training_config import TrainingConfig
 
     training_config = TrainingConfig.from_separate_configs(
-        conf_model=model_config, conf_splitter=splitter_config
+        conf_model=model_config,
+        conf_splitter=splitter_config,
+        features_dir=morphometrics_features_file.parent,
     )
 
     logger.info("Reading morphometrics features file.")
@@ -236,41 +238,54 @@ def run_training(
 
     make_torch_deterministic()
 
-    label_enc = LabelEncoder()
+    logger.info("Run training and evaluation.")
+
     x_features = df_morphometrics.drop(columns=["property|name", "filepath", "m_type"])
-    all_labels = label_enc.fit_transform(
-        df_morphometrics["m_type"]
-    )  # [0, 1, 2, 1, 1, ...]
-    labels_unique_str = sorted(df_morphometrics["m_type"])
+    logger.debug(f"- Input features : {x_features.columns.to_list()}")
+
+    # Convert to numeric labels: "UPC", "TPC-A", "TPC-B", ... -> 2, 0, 1, ...
+    label_enc = LabelEncoder()
+    label_enc.fit(df_morphometrics["m_type"])
+    y_labels = label_enc.transform(df_morphometrics["m_type"])  # 2, 0, 1, 1, 0, ...
+    labels_unique_str = label_enc.classes_  # "TPC-A", "TPC-B", "UPC"
+    logger.debug(f"- Classes        : {labels_unique_str}")
+
+    n_samples = len(x_features)
+    n_classes = len(np.unique(y_labels))
+    logger.debug(f"- N. of samples  : {n_samples}")
+    logger.debug(f"- N. of classes  : {n_classes}")
 
     if training_config.model_class.startswith("xgboost"):
-        training_config.model_params["num_class"] = len(labels_unique_str)
+        training_config.model_params["num_class"] = n_classes
         training_config.model_params["use_label_encoder"] = False  # suppress warning
     elif training_config.model_class.startswith("morphoclass"):
-        training_config.model_params["n_classes"] = len(labels_unique_str)
+        training_config.model_params["n_classes"] = n_classes
+    logger.debug(f"- Model class    : {training_config.model_class}")
 
     if training_config.seed is not None:
         reset_seeds(numpy_seed=training_config.seed, torch_seed=training_config.seed)
 
     splitter = training_config.splitter_cls(**training_config.splitter_params)
-    split = splitter.split(X=all_labels, y=all_labels)  # X doesn't matter
-    n_splits = splitter.get_n_splits(X=all_labels, y=all_labels)
+    split = splitter.split(X=x_features, y=y_labels)
+    n_splits = splitter.get_n_splits(X=x_features, y=y_labels)
+    logger.debug(f"- Splitter class  : {training_config.splitter_class}")
+    logger.debug(f"- N of splits     : {n_splits}")
 
-    probabilities = np.empty((len(all_labels), len(all_labels.unique())))
-    predictions = np.empty(len(all_labels), dtype=int)
+    probabilities = np.empty((n_samples, n_classes))
+    predictions = np.empty(n_samples, dtype=int)
 
     training_log = TrainingLog(config=training_config, labels_str=labels_unique_str)
     # SPLIT MODEL
     for n, (train_idx, val_idx) in enumerate(split):
         logger.info(f"Split {n + 1}/{n_splits}, ratio: {len(train_idx)}:{len(val_idx)}")
-        history = train_model(
-            training_config, train_idx, val_idx, x_features, all_labels
+        history = train_ml_model(
+            training_config, train_idx, val_idx, x_features, y_labels
         )
-        history["ground_truths"] = all_labels[val_idx]
+        history["ground_truths"] = y_labels[val_idx]
         history["train_idx"] = list(train_idx)
-        history["val_idx"] = list(val_idx)
+        history["valid_idx"] = list(val_idx)
 
-        all_labels[val_idx] = history["ground_truths"]
+        y_labels[val_idx] = history["ground_truths"]
         predictions[val_idx] = history["predictions"]
         probabilities[val_idx] = np.array(history["probabilities"])
 
@@ -278,21 +293,51 @@ def run_training(
         training_log.add_split(history)
 
     # collect results
-    training_log.set_y(all_labels, predictions, probabilities)
+    training_log.set_y(y_labels, predictions, probabilities)
 
     # MODEL ALL
     if training_config.train_all_samples:
         logger.info("Fit model on all samples")
-        train_idx = np.arange(len(all_labels))
+        train_idx = np.arange(len(y_labels))
 
-        history = train_model(
-            training_config, train_idx, val_idx, x_features, all_labels
+        history = train_ml_model(
+            training_config, train_idx, val_idx, x_features, y_labels
         )
         training_log.set_all_history(history)
 
     return training_log
 
 
-def train_model(config, train_idx, val_idx, x_features, all_labels) -> dict:
-    """Train a model."""
-    pass
+def train_ml_model(config, train_idx, valid_idx, x_features, y_labels):
+    """Train a sklearn-like model."""
+    import numpy as np
+
+    from morphoclass.training.cli import get_model
+
+    if not config.model_class.startswith(
+        "sklearn"
+    ) and not config.model_class.startswith("xgboost"):
+        raise ValueError(f"Training model of type {config.model_class} not supported.")
+
+    n_classes = len(np.unique(y_labels))
+    model = get_model(config, None, n_classes)
+
+    x_train, y_train = x_features[train_idx], y_labels[train_idx]
+    x_valid, y_valid = x_features[valid_idx], y_labels[valid_idx]
+
+    model.fit(x_train, y_train)
+
+    latent_features = x_features
+
+    probabilities = model.predict_proba(x_valid)
+    labels_val_pred = model.predict(x_valid)
+
+    history = {
+        "model": model,
+        "predictions": labels_val_pred,
+        "ground_truths": y_valid,
+        "probabilities": probabilities,
+        "latent_features": latent_features,
+    }
+
+    return history
