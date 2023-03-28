8ac7f0b3ef53ca7b44eed0fa1cc983452078052e
Delattre Emilie <emilie.delattre@epfl.ch>
2023-03-28 16:48:20 +0200

    Revert wrong change

--- src/morphoclass/transforms/augmentors/branching_only_neuron.py
+++ src/morphoclass/transforms/augmentors/branching_only_neuron.py
@@ -38,9 +38,9 @@ class BranchingOnlyNeuron:
             The modified morphology data sample.
         """
         neuron = data.tmd_neuron
-        neuron.apical = [tree.extract_simplified() for tree in neuron.apical_dendrite]
+        neuron.apical = [tree.extract_simplified() for tree in neuron.apical]
         neuron.axon = [tree.extract_simplified() for tree in neuron.axon]
-        neuron.basal = [tree.extract_simplified() for tree in neuron.basal_dendrite]
+        neuron.basal = [tree.extract_simplified() for tree in neuron.basal]
         neuron.undefined = [tree.extract_simplified() for tree in neuron.undefined]
 
         return data
