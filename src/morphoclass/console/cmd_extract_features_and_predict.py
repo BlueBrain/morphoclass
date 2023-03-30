7a5a18c0013fde379a905cb512e4bb6b3380e5ca
Delattre Emilie <emilie.delattre@epfl.ch>
2023-03-30 13:14:38 +0200

    Make isort happy

--- src/morphoclass/console/cmd_extract_features_and_predict.py
+++ src/morphoclass/console/cmd_extract_features_and_predict.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Implementation of the `morphoclass predict` CLI command."""
+from __future__ import annotations
+
 import logging
 import textwrap
 
